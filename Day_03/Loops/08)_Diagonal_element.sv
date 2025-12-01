7cf436a600ac80452bc26ae693a3759fef7f1e97
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-12-01 22:56:13 +0530

    Diagonal_element.sv

--- Day_03/Loops/08)_Diagonal_element.sv
+++ Day_03/Loops/08)_Diagonal_element.sv
@@ -3,7 +3,7 @@ module diagonal_print;
   int mat[4][4];
 
   initial begin
-    foreach(mat[i,j]) begin
+    foreach (mat[i,j]) begin
       mat[i][j] = i + j;
       if(i == j)
         $display("Diagonal: mat[%0d][%0d] = %0d", i, j, mat[i][j]);
