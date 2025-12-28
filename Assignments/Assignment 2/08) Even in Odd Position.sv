f38c8a4764f4182539be182eeaa7e7190b37ac4d
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-12-28 18:35:33 +0530

    Even in Odd Position

---
+++ Assignments/Assignment 2/08) Even in Odd Position.sv
@@ -0,0 +1,22 @@
+// Even in Odd Position
+class SeqGen;
+  rand int arr[8];
+
+  // even numbers in odd positions
+  constraint even_in_odd_pos {
+    foreach(arr[i])
+      (i % 2 == 0) -> (arr[i] % 2 == 0);}
+endclass
+
+module SeqGen_tb;
+  SeqGen s;
+
+  initial begin
+    s = new();
+    if(s.randomize()) begin
+      $display("Generated Sequence: %p", s.arr);
+    end else begin
+      $display("Randomization failed!");
+    end
+  end
+endmodule
