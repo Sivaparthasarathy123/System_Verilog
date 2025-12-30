a523adab6e873f7850f2879c41a293a3c199bd0a
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-12-30 23:40:44 +0530

    Distribution of values

---
+++ Assignments/Assignment 2/11) Distribution of values.sv
@@ -0,0 +1,29 @@
+// dist example 
+class distribution;
+    rand int unsigned data;
+
+    constraint data_dist_c {
+      data dist {
+        [0:100]   := 10,   
+        [101:255] := 30  
+      };
+    }
+  endclass
+
+module dist_tb;
+  distribution d;
+
+  initial begin
+    d = new();
+
+    $display("Randomized data samples:");
+
+    for (int i = 0; i < 10; i++) begin
+      if (d.randomize())
+        $display("Sample %0d: data = %0d", i+1, d.data);
+      else
+        $display("Randomization failed at sample %0d", i+1);
+    end
+  end
+
+endmodule
